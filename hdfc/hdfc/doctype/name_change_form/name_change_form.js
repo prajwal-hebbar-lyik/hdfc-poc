d1928f16b6fa28f183f20e7b23cc1963fc9b08a6
Hebbar <phebbar@lyik.com>
2024-01-26 19:12:35 +0000

    Created the Name Change Form Doctype

--- hdfc/hdfc/doctype/name_change_form/name_change_form.js
+++ hdfc/hdfc/doctype/name_change_form/name_change_form.js
@@ -1,13 +1,13 @@
 // Copyright (c) 2024, LYIK Technologies and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Consent Withdrawal Form", {
+// frappe.ui.form.on("Name Change Form", {
 // 	refresh(frm) {
 
 // 	},
 // });
 
-frappe.ui.form.on('Consent Withdrawal Form', {
+frappe.ui.form.on('Name Change Form', {
 	refresh: function(frm) {
 		//const full_name = frm.doc.full_name
 		frm.add_custom_button(__('Approve'), function() {
@@ -15,7 +15,7 @@ frappe.ui.form.on('Consent Withdrawal Form', {
 				'Are you sure to Approve?',
 				function() {
 					//this will run on yes
-					alert(frm.doc.full_name);
+					alert(frm.doc.customer_name);
 				},
 				function() {
 					//this will run on no
