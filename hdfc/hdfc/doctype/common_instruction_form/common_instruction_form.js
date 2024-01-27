fddcfcc3b7cf070eb11ab4aa6db50eb970a22a6a
Hebbar <phebbar@lyik.com>
2024-01-27 22:49:10 +0530

    Added custom button for Common Instructions Form

--- hdfc/hdfc/doctype/common_instruction_form/common_instruction_form.js
+++ hdfc/hdfc/doctype/common_instruction_form/common_instruction_form.js
@@ -6,3 +6,42 @@
 
 // 	},
 // });
+
+frappe.ui.form.on("Common Instruction Form", {
+  refresh: function (frm) {
+    //const full_name = frm.doc.full_name
+    frm.add_custom_button(
+      __("Approve"),
+      function () {
+        frappe.confirm(
+          "Are you sure to Approve?",
+          function () {
+            //this will run on yes
+            alert("frm.doc.customer_name");
+          },
+          function () {
+            //this will run on no
+            window.close();
+          }
+        );
+      },
+      __("ACTIONS")
+    );
+    frm.add_custom_button(
+      __("Reject"),
+      function () {
+        frappe.confirm(
+          "Are you sure to Reject?",
+          function () {
+            //this will run on yes
+          },
+          function () {
+            //this will run  on no
+            window.close();
+          }
+        );
+      },
+      __("ACTIONS")
+    );
+  },
+});
