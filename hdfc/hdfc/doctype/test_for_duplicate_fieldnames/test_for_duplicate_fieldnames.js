46002321883501264f935de982e82bd3be4009c3
Hebbar <prajwal.chaos@gmail.com>
2024-02-28 11:26:29 +0530

    Same fieldnames in 2 doctypes check

---
+++ hdfc/hdfc/doctype/test_for_duplicate_fieldnames/test_for_duplicate_fieldnames.js
@@ -0,0 +1,79 @@
+// Copyright (c) 2024, LYIK Technologies and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Test for Duplicate Fieldnames", {
+// 	refresh(frm) {
+
+// 	},
+// });
+
+// Copyright (c) 2024, LYIK Technologies and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Test for Duplicate Fieldnames", {
+// 	refresh(frm) {
+
+// 	},
+// });
+
+frappe.ui.form.on('Test for Duplicate Fieldnames', {
+  refresh: function(frm) {
+    let button;
+    if(frm.doc.status == "Approved") {
+      button = "Approved";
+    } else if (frm.doc.status == "Rejected") {
+      button = "Rejected";
+    } else {
+      button = "ACTIONS"
+    }
+    frm.add_custom_button(__('Approve'), function() {
+      frappe.confirm(
+        'Are you sure to Approve?',
+        function() {
+          const value = frm.doc.name
+          frappe.db.set_value('Test for Duplicate Fieldnames', value, {'status': 'Approved', 'reason': ""});
+          // frappe.db.set_value('Test for Duplicate Fieldnames', value, {'reason': ""});
+          frappe.msgprint(`Record ${value} has been Approved`);
+        },
+        function() {
+          // window.close();
+        }
+      );
+    }, __(button));
+
+    frm.add_custom_button(__('Reject'), function() {
+      frappe.confirm(
+        'Are you sure to Reject?',
+        function() {
+          const value = frm.doc.name
+          frappe.db.set_value('Test for Duplicate Fieldnames', value, {'status': 'Rejected'});
+          let d = new frappe.ui.Dialog({
+            title: 'Reasons for Rejecting the Form',
+            fields: [
+                {
+                    label: 'Reason',
+                    fieldname: 'reason',
+                    fieldtype: 'Text Editor'
+                }
+            ],
+            size: 'small', // small, large, extra-large 
+            primary_action_label: 'Submit',
+            primary_action(values) {
+                const value = frm.doc.name
+                frappe.db.set_value('Test for Duplicate Fieldnames', value, {'reason': values.reason})
+                d.hide();
+            }
+        });
+        
+        d.$wrapper.find('.modal-dialog').css("max-width", "60%");
+        d.show();
+          // frappe.msgprint(`Record ${value} has been rejected`);
+        },
+        function() {
+          // window.close();
+        }
+      );
+    }, __(button));
+
+  }
+})
